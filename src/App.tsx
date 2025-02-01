c86c6a9ff562d179ec01e9ae7a1e56a8621bfbc3
chrtmnn <c@hrtmnn.com>
2025-02-01 14:16:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PsTable from "./components/PsTable/PsTable";
+
+const App = () => {
+  return (
+    <>
+      <PsTable />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/PsTable/PsTable.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Table from "antd/es/table";
+
+export const PsTable: React.FC = () => {
+  return (
+    <>
+      <Table />
+    </>
+  );
+};
+
+export default PsTable;
